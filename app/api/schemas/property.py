9def6fc34b6e5ebc82b396f0456a242b39a32da0
Alexis Bozel <alexisbozelpro@gmail.com>
2024-12-12 14:35:24 +0100

    fix bug

--- app/api/schemas/property.py
+++ app/api/schemas/property.py
@@ -1,7 +1,4 @@
 from app.api.schemas.AbstractSchema import AbstractSchema
-from app.api.schemas.pricing import PricingBase
-from app.api.schemas.room import RoomBase
-
 
 class PropertyInputBase(AbstractSchema):
     idOwner: int
